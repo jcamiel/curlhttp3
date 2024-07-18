15e3c9afd167a10366246088a88b71a6de1852ad
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-07-18 12:18:08 +0200

    Add verbose

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-RUSTFLAGS="$RUSTFLAGS -A dead_code" cargo build
-target/debug/hurl integration/hurl/tests_ok/http_version_3_option.hurl
+RUSTFLAGS="-A dead_code" cargo build
+target/debug/hurl --verbose integration/hurl/tests_ok/http_version_3_option.hurl
