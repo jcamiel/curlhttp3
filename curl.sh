5b6de2861f6d1349415e0a8d61befbadf9052f80
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-07-24 13:29:32 +0200

    Add hurl 5.0.0 from generic.

---
+++ curl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <url> <number_of_times> <sleep_interval_in_seconds>"
+    exit 1
+fi
+
+curl --version
+ldd /usr/bin/curl
+
+# Assign arguments to variables
+URL=$1
+N=$2
+SLEEP_INTERVAL=$3
+
+
+# Loop N times
+for (( i=1; i<=N; i++ ))
+do
+    echo "Request #$i:"
+
+    # Perform the curl request and display the HTTP status code and time
+    curl --head --http3 -s -o /dev/null -w "HTTP Status: %{http_code}, Time: %{time_total} s\n" "$URL"
+
+    # Sleep between requests if not the last request
+    if [ "$i" -lt "$N" ]; then
+        sleep "$SLEEP_INTERVAL"
+    fi
+done
+
+echo "Completed $N requests to $URL."
---
+++ hurl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <url> <number_of_times> <sleep_interval_in_seconds>"
+    exit 1
+fi
+
+bin/hurl --version
+ldd bin/hurl
+
+# Assign arguments to variables
+URL=$1
+N=$2
+SLEEP_INTERVAL=$3
+
+
+# Loop N times
+for (( i=1; i<=N; i++ ))
+do
+    echo "Request #$i:"
+
+    # Perform the curl request and display the HTTP status code and time
+    echo "HEAD $URL" | bin/hurl --http3 --verbose
+
+    # Sleep between requests if not the last request
+    if [ "$i" -lt "$N" ]; then
+        sleep "$SLEEP_INTERVAL"
+    fi
+done
+
+echo "Completed $N requests to $URL."
