107312b42c8d3684b593679d66e573931f4c136e
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-07-24 17:37:20 +0200

    Wip

--- setup_build_hurl.sh
+++ setup_build_hurl.sh
@@ -4,5 +4,4 @@ set -Eeuo pipefail
 pacman --noconfirm -Syu git curl
 pacman --noconfirm -Syu pkgconf gcc glibc openssl libxml2
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH=$HOME/.cargo/bin:$PATH
 git clone https://github.com/Orange-OpenSource/hurl.git
