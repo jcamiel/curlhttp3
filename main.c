10e039aa54b84ddf480af157161c96a85ba0a5d1
Jean-Christophe Amiel <jeanchristophe.amiel@gmail.com>
2024-07-26 16:12:46 +0200

    Update C sampke with CURLOPT_SSL_CTX_FUNCTION/CURLOPT_SSL_CTX_DATA

--- main.c
+++ main.c
@@ -1,30 +1,28 @@
 #include <curl/curl.h>
 
-int main(int argc, char *argv[])
-{
+static CURLcode sslctx_function(CURL *curl, void *sslctx, void *parm) {
+    return CURLE_OK;
+}
+
+int main(int argc, char *argv[]) {
     CURLcode ret;
     CURL *hnd;
+
+    char *data = "";
+
 
     hnd = curl_easy_init();
 
     curl_easy_reset(hnd);
 
-    curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 102400L);
     curl_easy_setopt(hnd, CURLOPT_URL, "https://google.com");
     curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
     curl_easy_setopt(hnd, CURLOPT_NOBODY, 1L);
-    curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/8.8.0");
     curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
     curl_easy_setopt(hnd, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_3);
-    curl_easy_setopt(hnd, CURLOPT_FILETIME, 1L);
     curl_easy_setopt(hnd, CURLOPT_VERBOSE, 1L);
-    curl_easy_setopt(hnd, CURLOPT_FTP_SKIP_PASV_IP, 1L);
-
-    curl_easy_setopt(hnd, CURLOPT_COOKIEFILE, "");
-    curl_easy_setopt(hnd, CURLOPT_CERTINFO, 1L);
-    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 1L);
-    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 1L);
-
+    curl_easy_setopt(hnd, CURLOPT_SSL_CTX_FUNCTION, *sslctx_function);
+    curl_easy_setopt(hnd, CURLOPT_SSL_CTX_DATA, data);
     ret = curl_easy_perform(hnd);
 
     curl_easy_cleanup(hnd);
